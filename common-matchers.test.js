6f08edf5c4689eb36351706f7a0e1371ef086e5a
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-04 16:28:36 +0100

    Matchers for strings

--- common-matchers.test.js
+++ common-matchers.test.js
@@ -63,3 +63,18 @@ test('two plus two', () => {
     //expect(value).toBe(0.3);           This won't work because of rounding error
     expect(value).toBeCloseTo(0.3);   // This works.
   });
+
+
+// STRINGS => .toMatch (works with regex)
+
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+
+// ARRAYS AND OTHER ITERABLES => .toContain
+
