cd6dbb38a5272ec0bfe950714c6521a997dc6481
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-04 16:30:53 +0100

    Matchers for exceptions

--- common-matchers.test.js
+++ common-matchers.test.js
@@ -91,3 +91,19 @@ test('the shopping list has milk on it', () => {
   expect(new Set(shoppingList)).toContain('milk');
 });
 
+
+// EXCEPTIONS => .toThrow
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
+
